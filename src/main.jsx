a58b62524d35955d508b390a00bdf0bf6b717a1c
Rahasmi <raziahmadhasmi786@gmail.com>
2024-04-22 09:54:19 -0700

    setup of axios instance

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
 
 //CSS imports
 import './index.css';
-// Library imports
+
 import ReactDOM from 'react-dom/client'
+import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-//Components imports
 import App from './App.jsx';
-import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
   <BrowserRouter>
     <App />
     <Toaster/>
   </BrowserRouter>
-    
+  </Provider>    
 );
